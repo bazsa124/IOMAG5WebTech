e7c3148f7d8eb7c438c1c0e8c60b5d7aa36afb09
bazsa124 <40303378+bazsa124@users.noreply.github.com>
2024-03-25 09:36:35 +0100

    Update
    Add video1, comlete video

---
+++ IOMAG5_0311/megoldas.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Feldolgozo</title>
+    </head>
+    <body>
+        <font style="font-size:18pt">
+            <h2>HTML 5 urlap_IOMAG5</h2>
+
+            <?php
+
+                echo ("<strong> Nev: </strong>" .POST['name']. "<br><br>");
+
+                echo ("<strong> PIN: </strong>" .POST['pin']. "<br><br>");
+
+                echo ("<strong> Gyumolcs: </strong>" .POST['fruit']. "<br><br>");
+
+                echo ("<strong> Eletkor: </strong>" .POST['age']. "<br><br>");
+
+                echo ("<strong> Labmeret: </strong>" .POST['foot']. "<br><br>");
+
+                echo ("<strong> Onbizalom: </strong>" .POST['onbiz']. "<br><br>");
+                
+            ?>
+            <a href="IOMAG5_urlap.html">Vissza az urlapra</a>
+        </font>
+    </body>
+</html>
